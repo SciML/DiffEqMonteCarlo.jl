8ac9c4cad7e50f35ef068139be053697b770bd0b
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-07 00:29:46 -0700

    add the solve to here

--- src/DiffEqMonteCarlo.jl
+++ src/DiffEqMonteCarlo.jl
@@ -2,4 +2,17 @@ __precompile__()
 
 module DiffEqMonteCarlo
 
+using DiffEqBase
+
+import DiffEqBase: solve
+
+function solve(prob::AbstractMonteCarloProblem,alg;num_monte=10000,kwargs...)
+  elapsedTime = @elapsed solution_data = pmap((i)-> begin
+    new_prob = prob.prob_func(deepcopy(prob.prob),i)
+    prob.output_func(solve(new_prob,alg;kwargs...))
+  end,1:num_monte)
+  solution_data = convert(Array{typeof(solution_data[1])},solution_data)
+  return(MonteCarloSolution(solution_data,elapsedTime))
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,4 @@
+using DiffEqMonteCarlo
+using Base.Test
 
+@time @testset "Monte Carlo Simulations" begin include("monte.jl") end
