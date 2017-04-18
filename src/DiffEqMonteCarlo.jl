00112dff821428a2f7941064665943724f429399
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-17 18:31:50 -0700

    output func adds i

--- src/DiffEqMonteCarlo.jl
+++ src/DiffEqMonteCarlo.jl
@@ -7,24 +7,25 @@ using DiffEqBase
 import DiffEqBase: solve
 
 function solve(prob::AbstractMonteCarloProblem,alg::DEAlgorithm;num_monte=10000,parallel_type=:pmap,kwargs...)
+  
   if parallel_type == :pmap
     elapsedTime = @elapsed solution_data = pmap((i)-> begin
       new_prob = prob.prob_func(deepcopy(prob.prob),i)
-      prob.output_func(solve(new_prob,alg;kwargs...))
+      prob.output_func(solve(new_prob,alg;kwargs...),i)
     end,1:num_monte)
     solution_data = convert(Array{typeof(solution_data[1])},solution_data)
 
   elseif parallel_type == :parfor
     elapsedTime = @elapsed solution_data = @sync @parallel (vcat) for i in 1:num_monte
       new_prob = prob.prob_func(deepcopy(prob.prob),i)
-      prob.output_func(solve(new_prob,alg;kwargs...))
+      prob.output_func(solve(new_prob,alg;kwargs...),i)
     end
 
   elseif parallel_type == :threads
     solution_data = Vector{Any}()
     elapsedTime = @elapsed Threads.@threads for i in 1:num_monte
       new_prob = prob.prob_func(deepcopy(prob.prob),i)
-      push!(solution_data,prob.output_func(solve(new_prob,alg;kwargs...)))
+      push!(solution_data,prob.output_func(solve(new_prob,alg;kwargs...),i))
     end
     solution_data = convert(Array{typeof(solution_data[1])},solution_data)
 
@@ -32,7 +33,7 @@ function solve(prob::AbstractMonteCarloProblem,alg::DEAlgorithm;num_monte=10000,
     solution_data = Vector{Any}()
     elapsedTime = @elapsed for i in 1:num_monte
       new_prob = prob.prob_func(deepcopy(prob.prob),i)
-      push!(solution_data,prob.output_func(solve(new_prob,alg;kwargs...)))
+      push!(solution_data,prob.output_func(solve(new_prob,alg;kwargs...),i))
     end
     solution_data = convert(Array{typeof(solution_data[1])},solution_data)
 
