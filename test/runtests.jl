b9033048aea0e09950f6c28ddfee24b039563e13
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-06 23:33:13 -0700

    add the analysis tools to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using DiffEqMonteCarlo
 using Base.Test
 
 @time @testset "Monte Carlo Simulations" begin include("monte.jl") end
+@time @testset "Monte Carlo Analysis" begin include("analysis.jl") end
